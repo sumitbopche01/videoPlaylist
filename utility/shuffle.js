91629ccdde531a4166db31261bf4d44ddaafd0cb
Sumit Bopche <sumitbopche01@gmail.com>
2019-03-18 00:15:07 +0530

    added shuffle functionality in utility folder

---
+++ utility/shuffle.js
@@ -0,0 +1,20 @@
+const shuffle = (playlist) => {
+    
+    let currentIndex = playlist.length;
+    let randomIndex = 0;
+    let temp ;
+    while(currentIndex !== 0){
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        temp = playlist[currentIndex];
+        playlist[currentIndex] = playlist[randomIndex];
+        playlist[randomIndex] = temp;
+    }
+    
+    return playlist;
+    
+}
+
+module.exports = {
+    shuffle,
+}
